9d1040c207dfcea7ee031b182f6ec80438ac0613
Patrick Schönfeld <schoenfeld@nexus-netsoft.com>
2019-08-21 14:31:32 +0200

    + permission fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -58,7 +58,7 @@ set -xe
 # Configure PHP
 j2 /usr/local/etc/php/php.ini.j2 > /usr/local/etc/php/php.ini
 
-chown -R www-data:www-data /data/
+chown -R 1000:1000 /data/
 
 #      # Disable maintenance mode to validate LetsEncrypt certificates
 #      test -f /maintenance_on.flag && rm /maintenance_on.flag
